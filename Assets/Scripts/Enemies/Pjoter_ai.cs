76efbf694c746db472a1d5e3af3ca5e78db0fbdc
Piotr Krawiec <piotr.krawiec23@gmail.com>
2018-08-17 14:59:51 +0200

    finished lvl 5

--- Assets/Scripts/Enemies/Pjoter_ai.cs
+++ Assets/Scripts/Enemies/Pjoter_ai.cs
@@ -13,63 +13,73 @@ public class Pjoter_ai : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Vector2 player_position = collision.collider.transform.position;
-
-        
-        Vector2 player_size = collision.collider.GetComponent<BoxCollider2D>().size * collision.collider.gameObject.transform.localScale;
-        
-
-        player_position.y -= player_size.y/2 - 0.1f;
-
-        Vector2 enemy_position = transform.position;
-
-        Vector2 enemy_size = gameObject.GetComponent<BoxCollider2D>().size * gameObject.transform.localScale;
-
-        enemy_position.y += enemy_size.y/2;
-
-
-        if (player_position.y >= enemy_position.y)
+        if (collision.gameObject.name.Equals("Player"))
         {
-            Destroy(gameObject);
-            collision.collider.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            collision.collider.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 500);
-        } else
-        {
-            if(collision.collider.tag.Equals("Player")) {
-                collision.gameObject.GetComponent<Player_Health>().TakeDamage(34);
+            Vector2 player_position = collision.collider.transform.position;
+
+
+            Vector2 player_size = collision.collider.GetComponent<BoxCollider2D>().size * collision.collider.gameObject.transform.localScale;
+
+
+            player_position.y -= player_size.y / 2 - 0.1f;
+
+            Vector2 enemy_position = transform.position;
+
+            Vector2 enemy_size = gameObject.GetComponent<BoxCollider2D>().size * gameObject.transform.localScale;
+
+            enemy_position.y += enemy_size.y / 2;
+
+
+            if (player_position.y >= enemy_position.y)
+            {
+                Destroy(gameObject);
+                collision.collider.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                collision.collider.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 500);
             }
-                
+            else
+            {
+                if (collision.collider.tag.Equals("Player"))
+                {
+                    collision.gameObject.GetComponent<Player_Health>().TakeDamage(34);
+                }
+
+            }
         }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-         Vector2 player_position = collision.collider.transform.position;
-
-        
-        Vector2 player_size = collision.collider.GetComponent<BoxCollider2D>().size * collision.collider.gameObject.transform.localScale;
-        
-
-        player_position.y -= player_size.y/2 - 0.1f;
-
-        Vector2 enemy_position = transform.position;
-
-        Vector2 enemy_size = gameObject.GetComponent<BoxCollider2D>().size * gameObject.transform.localScale;
-
-        enemy_position.y += enemy_size.y/2;
-
-
-        if (player_position.y >= enemy_position.y)
+        if (collision.gameObject.name.Equals("Player"))
         {
-            Destroy(gameObject);
-            collision.collider.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            collision.collider.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 500);
-        } else
-        {
-            if(collision.collider.tag.Equals("Player")) {
-                collision.gameObject.GetComponent<Player_Health>().TakeDamage(34);
+            Vector2 player_position = collision.collider.transform.position;
+
+
+            Vector2 player_size = collision.collider.GetComponent<BoxCollider2D>().size * collision.collider.gameObject.transform.localScale;
+
+
+            player_position.y -= player_size.y / 2 - 0.1f;
+
+            Vector2 enemy_position = transform.position;
+
+            Vector2 enemy_size = gameObject.GetComponent<BoxCollider2D>().size * gameObject.transform.localScale;
+
+            enemy_position.y += enemy_size.y / 2;
+
+
+            if (player_position.y >= enemy_position.y)
+            {
+                Destroy(gameObject);
+                collision.collider.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                collision.collider.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 500);
             }
-                
+            else
+            {
+                if (collision.collider.tag.Equals("Player"))
+                {
+                    collision.gameObject.GetComponent<Player_Health>().TakeDamage(34);
+                }
+
+            }
         }
     }
 
--- Assets/Scripts/Enemies/Preclopies_ai.cs
+++ Assets/Scripts/Enemies/Preclopies_ai.cs
@@ -33,7 +33,8 @@ public class Preclopies_ai : MonoBehaviour
             ChangeDirection();
             if (left.collider.tag.Equals("Enemy"))
             {
-                left.collider.gameObject.GetComponent<Preclopies_ai>().ChangeDirection();
+                if(left.collider.gameObject.GetComponent<Preclopies_ai>() != null)
+                    left.collider.gameObject.GetComponent<Preclopies_ai>().ChangeDirection();
             }
 
         }
@@ -42,7 +43,8 @@ public class Preclopies_ai : MonoBehaviour
             ChangeDirection();
             if (right.collider.tag.Equals("Enemy"))
             {
-                right.collider.gameObject.GetComponent<Preclopies_ai>().ChangeDirection();
+               if (right.collider.gameObject.GetComponent<Preclopies_ai>() != null)
+                    right.collider.gameObject.GetComponent<Preclopies_ai>().ChangeDirection();
             }
 
         }
@@ -63,63 +65,73 @@ public class Preclopies_ai : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Vector2 player_position = collision.collider.transform.position;
-
-        
-        Vector2 player_size = collision.collider.GetComponent<BoxCollider2D>().size * collision.collider.gameObject.transform.localScale;
-        
-
-        player_position.y -= player_size.y/2 - 0.1f;
-
-        Vector2 enemy_position = transform.position;
-
-        Vector2 enemy_size = gameObject.GetComponent<BoxCollider2D>().size * gameObject.transform.localScale;
-
-        enemy_position.y += enemy_size.y/2;
-
-
-        if (player_position.y >= enemy_position.y)
+        if (collision.gameObject.name.Equals("Player"))
         {
-            Destroy(gameObject);
-            collision.collider.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            collision.collider.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 500);
-        } else
-        {
-            if(collision.collider.tag.Equals("Player")) {
-                collision.gameObject.GetComponent<Player_Health>().TakeDamage(34);
+            Vector2 player_position = collision.collider.transform.position;
+
+
+            Vector2 player_size = collision.collider.GetComponent<BoxCollider2D>().size * collision.collider.gameObject.transform.localScale;
+
+
+            player_position.y -= player_size.y / 2 - 0.1f;
+
+            Vector2 enemy_position = transform.position;
+
+            Vector2 enemy_size = gameObject.GetComponent<BoxCollider2D>().size * gameObject.transform.localScale;
+
+            enemy_position.y += enemy_size.y / 2;
+
+
+            if (player_position.y >= enemy_position.y)
+            {
+                Destroy(gameObject);
+                collision.collider.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                collision.collider.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 500);
             }
-                
+            else
+            {
+                if (collision.collider.tag.Equals("Player"))
+                {
+                    collision.gameObject.GetComponent<Player_Health>().TakeDamage(34);
+                }
+
+            }
         }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-         Vector2 player_position = collision.collider.transform.position;
-
-        
-        Vector2 player_size = collision.collider.GetComponent<BoxCollider2D>().size * collision.collider.gameObject.transform.localScale;
-        
-
-        player_position.y -= player_size.y/2 - 0.1f;
-
-        Vector2 enemy_position = transform.position;
-
-        Vector2 enemy_size = gameObject.GetComponent<BoxCollider2D>().size * gameObject.transform.localScale;
-
-        enemy_position.y += enemy_size.y/2;
-
-
-        if (player_position.y >= enemy_position.y)
+        if (collision.gameObject.name.Equals("Player"))
         {
-            Destroy(gameObject);
-            collision.collider.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            collision.collider.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 500);
-        } else
-        {
-            if(collision.collider.tag.Equals("Player")) {
-                collision.gameObject.GetComponent<Player_Health>().TakeDamage(34);
+            Vector2 player_position = collision.collider.transform.position;
+
+
+            Vector2 player_size = collision.collider.GetComponent<BoxCollider2D>().size * collision.collider.gameObject.transform.localScale;
+
+
+            player_position.y -= player_size.y / 2 - 0.1f;
+
+            Vector2 enemy_position = transform.position;
+
+            Vector2 enemy_size = gameObject.GetComponent<BoxCollider2D>().size * gameObject.transform.localScale;
+
+            enemy_position.y += enemy_size.y / 2;
+
+
+            if (player_position.y >= enemy_position.y)
+            {
+                Destroy(gameObject);
+                collision.collider.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                collision.collider.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 500);
             }
-                
+            else
+            {
+                if (collision.collider.tag.Equals("Player"))
+                {
+                    collision.gameObject.GetComponent<Player_Health>().TakeDamage(34);
+                }
+
+            }
         }
     }
 
